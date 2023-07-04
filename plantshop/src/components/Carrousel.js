e51e93949440eccefe0f8ebf28243de9bbca6455
Franco Parodi <francoparodi2001@gmail.com>
2023-07-04 20:18:37 -0300

    Mensaje de confirmación

---
+++ plantshop/src/components/Carrousel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import naranja from '../assets/naranja.png'
+import palta from '../assets/palta.png'
+import limon from '../assets/limones.png'
+import cerco from '../assets/bannerCerco.png'
+
+function Carrousel() {
+  return (
+    <div className='carrouselContainer'>
+      <div className='carrouselInterno'> 
+      <Carousel>
+      <div className='contenedor-imgcarrousel'>
+        <img src={palta} className='imgCarrousel' alt="Maceta colgante"/>
+        <img src={limon} className='imgCarrousel' alt="Maceta colgante"/>
+        <img src={naranja} className='imgCarrousel' alt="Maceta colgante"/>
+      </div>
+      <div>
+        <img src={cerco} alt="Otra maceta colgante" />
+      </div>
+      {/* Agrega más imágenes según tus necesidades */}
+    </Carousel>
+      </div>
+    </div>
+      );
+    }
+
+export default Carrousel;
---
+++ plantshop/src/components/Search.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { data } from '../mocks/mocks.js';
+
+const Search = () => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    console.log("search ", searchTerm);
+  };
+
+  return (
+    <div className='search-container'>
+      <Form className="d-flex buscador">
+        <div className='form-container'>
+          <Form.Control
+            type="search"
+            placeholder="Busca tus productos"
+            className="me-2 botonBusqueda"
+            id="form-control"
+            aria-label="Search"
+            value={value}
+            onChange={onChange}
+          />
+          <div>
+          <div className="dropdown">
+            {data.filter((item) => {
+                const searchTerm = value.toLowerCase();
+                const name = item.name.toLowerCase();
+                  return (
+                      searchTerm &&name.startsWith(searchTerm) &&name !== searchTerm)})
+                    .slice(0, 2).map((item) => (
+                    <ul onClick={() => onSearch(item.name)} className="dropdown-row"key={item.name}>
+                      {item.name}
+                    </ul>
+              ))}
+          </div>
+        </div>
+        </div>
+        <Button variant="outline-success" id='botonSearch' onClick={() => onSearch(value)}>
+          Search
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Search;
