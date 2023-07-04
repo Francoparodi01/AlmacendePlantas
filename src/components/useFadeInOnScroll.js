c80961ef65d80137f797b2c6b21e83c0fbc573e2
Franco Parodi <francoparodi2001@gmail.com>
2023-07-04 20:35:32 -0300

    arreglando cosas

---
+++ src/components/useFadeInOnScroll.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export const useFadeInOnScroll = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    
+
+
+    useEffect(() => {
+        
+        const checkScroll = () => {
+            if (window.scrollY > 1) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+        
+
+        window.addEventListener('scroll', checkScroll);
+        return () => window.removeEventListener('scroll', checkScroll);
+    }, []);
+
+    return isVisible ? 'fade-in' : '';
+};
