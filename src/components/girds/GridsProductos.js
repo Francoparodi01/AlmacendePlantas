fc63cfc17b19b8e355ae3aa335e9eccebe3c9f00
unknown <francoparodi2001@gmail.com>
2023-08-01 12:58:10 -0300

    merge branches

--- src/components/girds/GridsProductos.js
+++ src/components/girds/GridsProductos.js
@@ -1,22 +1,14 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import ItemList from '../productos/ItemList'
-<<<<<<< HEAD
-import FiltradoProductos from '../productos/FormProductos'
-=======
 import FormProductos from '../productos/FormProductos'
->>>>>>> master
 
 const GridsProductos = () => {
   return (
     <div>
         <Container className='containerGrid'>
           <Row className='row-productos'>
-<<<<<<< HEAD
-            <Col className='columna-filtros'><FiltradoProductos/></Col>
-=======
             <Col className='columna-filtros'><FormProductos/></Col>
->>>>>>> master
             <Col xs className='col-container-productos'><ItemList/></Col>
           </Row>
         </Container>
